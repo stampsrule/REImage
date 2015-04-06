d17dc47427e7e1b6b5f19f566c9c1d4a3dfcc071
stampsrule <stampsrule@gmail.com>
2015-04-06 13:25:05 -0600

    Initial Commit

---
+++ REImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  REImage
+//
+//  Created by Daniel Bell on 2015-04-06.
+//  Copyright (c) 2015 BellTechMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ REImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  REImage
+//
+//  Created by Daniel Bell on 2015-04-06.
+//  Copyright (c) 2015 BellTechMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
